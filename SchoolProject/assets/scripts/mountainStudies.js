c0bbfa5ed03967931055c40ebc73051a951864f8
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-11-18 16:16:09 +0100

    js hiker ok, popup notok

--- SchoolProject/assets/scripts/mountainStudies.js
+++ SchoolProject/assets/scripts/mountainStudies.js
@@ -1,12 +1,36 @@
 
 const hiker = document.querySelector(".hiker");
 // console.log(hiker);
-const dates = document.querySelectorAll(".dates");
-// console.log(dates);
+const dates = document.querySelectorAll(".date");
+console.log(dates);
 
 dates.forEach(date => {
-    console.log("coucou");
+    // console.log("coucou");
     date.addEventListener("click", function(){
-        hiker.style.gridArea = "top1";
+        if(date.classList == "date top1"){
+            hiker.style.transitionDuration = "0.3s";
+            hiker.style.transitionTimingFunction= "ease-in-out";
+            hiker.style.gridArea = "top1";
+            hiker.style.justifySelf = "end";
+            alert("2007 - Institut des Dames de Marie - CESS 'Grande Distinction'");
+        }
+        else if(date.classList == "date top2"){
+            hiker.style.transitionDuration = "0.3s";
+            hiker.style.transitionTimingFunction= "ease-in-out";
+            hiker.style.gridArea = "top2";
+            hiker.style.justifySelf = "end";
+        }
+        else if(date.classList == "date bottom1"){
+            hiker.style.transitionDuration = "0.3s";
+            hiker.style.transitionTimingFunction= "ease-in-out";
+            hiker.style.gridArea = "bottom1";
+            hiker.style.justifySelf = "center";
+        }
+        else if(date.classList == "date bottom2"){
+            hiker.style.transitionDuration = "0.3s";
+            hiker.style.transitionTimingFunction= "ease-in-out";
+            hiker.style.gridArea = "bottom2";
+            hiker.style.justifySelf = "center";
+        }
     })
 });
