8948bcfcd930daf14d4ebd18bbf979245a393c51
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-11-19 11:17:29 +0100

    studies page ok init workPage

--- SchoolProject/assets/scripts/mountainStudies.js
+++ SchoolProject/assets/scripts/mountainStudies.js
@@ -2,36 +2,109 @@
 const hiker = document.querySelector(".hiker");
 // console.log(hiker);
 const dates = document.querySelectorAll(".date");
-console.log(dates);
+// console.log(dates);
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 dates.forEach(date => {
-    // console.log("coucou");
+    // console.log(date.textContent);
     date.style.cursor = "pointer";
     date.addEventListener("click", function(){
         if(date.classList == "date top1"){
-            hiker.style.transitionDuration = "0.3s";
-            hiker.style.transitionTimingFunction= "ease-in-out";
-            hiker.style.gridArea = "top1";
-            hiker.style.justifySelf = "end";
-            // alert("2007 - Institut des Dames de Marie - CESS 'Grande Distinction'");
+            moveHiker("top1", "end");
+            displayModal(
+                date.textContent,
+                "CESS - Grande Distinction",
+                "Institut des Dames de Marie - Woluwé-Saint-Lambert", 
+                "Sciences sociales - Latin - Langues");
         }
         else if(date.classList == "date top2"){
-            hiker.style.transitionDuration = "0.3s";
-            hiker.style.transitionTimingFunction= "ease-in-out";
-            hiker.style.gridArea = "top2";
-            hiker.style.justifySelf = "end";
+            moveHiker("top2", "end");
+            displayModal(
+                date.textContent,
+                "Certificat Gestion de Base",
+                "Bruxelles Formation - Bruxelles",
+                "Gestion de Base"
+                );
+
         }
         else if(date.classList == "date bottom1"){
-            hiker.style.transitionDuration = "0.3s";
-            hiker.style.transitionTimingFunction= "ease-in-out";
-            hiker.style.gridArea = "bottom1";
-            hiker.style.justifySelf = "center";
+            moveHiker("bottom1","center");
+            displayModal(
+                date.textContent,
+                "",
+                "Institut Libre Marie Haps - Ixelles",
+                "Logopédie"
+            )
         }
         else if(date.classList == "date bottom2"){
-            hiker.style.transitionDuration = "0.3s";
-            hiker.style.transitionTimingFunction= "ease-in-out";
-            hiker.style.gridArea = "bottom2";
-            hiker.style.justifySelf = "center";
+
+            moveHiker("bottom2","center");
+            displayModal(
+                date.textContent,
+                "CEE...",
+                "Interface 3 - Bruxelles", 
+                "Web Developer Application");
         }
     })
 });
+
+
+function moveHiker(gridArea, justifySelf, alignSelf = "center"){
+    hiker.style.gridArea = gridArea;
+    hiker.style.justifySelf = justifySelf;
+    hiker.style.alignSelf = alignSelf;
+}
+
+function displayModal(date, certificate = "", location, description){
+    // First we clear the div
+    modalContent.innerHTML= "";
+
+    // Then we create elements
+    const pDate = document.createElement("p");
+    const pCertificate = document.createElement("p");
+    const pLocation = document.createElement("p");
+    const pDescription = document.createElement("p");
+    
+    // Close button
+    const close = document.createElement("span")
+    close.innerHTML = "&times;";
+    close.setAttribute("class","close");
+    close.addEventListener("click", function(){
+        modal.style.display = "none";
+    })
+
+    // Element contents
+    pDate.textContent = date;
+    pDate.style.fontFamily = "Dancing Script";
+    pDate.style.fontWeight = "bold";
+    pDate.style.textAlign = "center";
+
+    pCertificate.textContent = certificate;
+    pCertificate.style.fontWeight = "bold";
+    pCertificate.style.textAlign = "center";
+
+    pLocation.textContent = location;
+    pLocation.style.fontFamily = "Poppins";
+    pLocation.style.textAlign = "center";
+
+    pDescription.textContent = description;
+    pDescription.style.fontStyle = "italic";
+    pDescription.style.fontFamily = "Poppins";
+    pDescription.style.textAlign = "center";
+
+    // We add it to the div
+    modalContent.appendChild(close);
+    modalContent.appendChild(pDate);
+    modalContent.appendChild(pCertificate);
+    modalContent.appendChild(pLocation);
+    modalContent.appendChild(pDescription);
+    // console.log(modal);
+    modal.style.display = "block";
+}
