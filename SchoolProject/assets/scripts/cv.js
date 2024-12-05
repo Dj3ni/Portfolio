51487e9c89d3ddd769ed7138fa171f1db6f65d05
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2024-12-05 16:08:15 +0100

    pdf script ok

--- SchoolProject/assets/scripts/cv.js
+++ SchoolProject/assets/scripts/cv.js
@@ -2,7 +2,19 @@ const CV_BUTTON = document.getElementById("cv");
 console.log(CV_BUTTON);
 const PDF_DIV = document.querySelector(".pdf")
 console.log(PDF_DIV);
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
 
-CV_BUTTON.addEventListener("click", function(){
-    PDF_DIV.style.display = "block";
+
+window.onclick = function(event) {
+    if (!PDF_DIV.contains(event.target) && event.target !== CV_BUTTON) {
+        PDF_DIV.classList.remove("visible"); // Retire la classe "visible"
+    }
+}
+
+CV_BUTTON.addEventListener("click", function(event){
+    event.preventDefault();
+    PDF_DIV.classList.toggle("visible");
+    // console.log(PDF_DIV);
 })
+
