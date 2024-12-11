797a043d0914e4ca3e69e949096e8a36a7f300d4
Jen <fernandezgarcia.jenny@gmail.com>
2024-12-11 17:41:39 +0100

    bubble contact ok

--- SchoolProject/assets/scripts/speechBubbles.js
+++ SchoolProject/assets/scripts/speechBubbles.js
@@ -2,17 +2,17 @@ const Internship = "I'm searching for an internship as web developer";
 const InternshipDates = "From 5th of May 2025 to 28th of June 2025";
 const TypingSpeed = 100;
 
-/* Home Page */ 
+/***************** Home Page ******************/ 
 
 // Left bubble
 const textLeft = document.getElementById("text-left");
 const bubbleLeft = document.getElementById("bubble-left");
-console.log(bubbleLeft);
+// console.log(bubbleLeft);
 
-// Rigth bubble
+// Right bubble
 const textRight = document.getElementById("text-rigth");
 const bubbleRight = document.getElementById("bubble-right");
-console.log(bubbleRight);
+// console.log(bubbleRight);
 
 function typeText(text, element, callback){
     let i = 0;
@@ -27,6 +27,8 @@ function typeText(text, element, callback){
         }
     }, TypingSpeed);
 }
+
+// Function for HomePage
 
 function animateBubbles(){
     // Init
@@ -48,7 +50,7 @@ function animateBubbles(){
                         bubbleLeft.classList.remove("seen");
                         bubbleRight.classList.remove("seen");
                         // start this loop again
-                        setTimeout(animateBubbles, 1000);
+                        setTimeout(animateBubbles, 2000);
                     },5000);
                 });
             },1000)
@@ -56,19 +58,56 @@ function animateBubbles(){
     }, 500);
 }
 
-animateBubbles();
+window.addEventListener("load", animateBubbles);
+
+
+// animateBubbles();
+
+
+/*************** Contact Page ****************/ 
+
+const phrases = [Internship, InternshipDates];
+const PirateBubble = document.getElementById('speech-bubble');
+const BubbleContent = document.getElementById("bubble-content");
+// console.log(PirateBubble);
+// console.log(BubbleContent);
+
+function bubblePirate(){
+    PirateBubble.classList.remove("seen");
+    BubbleContent.textContent = "";
+    setTimeout(()=>{
+        // Display bubble
+        PirateBubble.classList.add("seen");
+        displayNextPhrase();
+    },500)
+}
+
+let currentPhrase = 0;
+function displayNextPhrase() {
+    if (currentPhrase < phrases.length) {
+        BubbleContent.textContent = "";
+        typeText(phrases[currentPhrase], BubbleContent, () => {
+            currentPhrase++;
+            setTimeout(displayNextPhrase, 1000); 
+        });
+    } else {
+        setTimeout(() => {
+            currentPhrase = 0;
+            PirateBubble.classList.remove("seen");
+            setTimeout(bubblePirate, 500);
+        }, 4000);
+    }
+}
+
+window.addEventListener("load", bubblePirate);
+
+// bubblePirate();
 
 
 
 
-
-
-
-
-
-
-
-// I want to diplay the text progressively
+// Test function
+//  I want to diplay the text progressively
 
 // let i = 0;
 
@@ -77,6 +116,5 @@ animateBubbles();
 //         textLeft.textContent += Internship[i];
 //         i++;
 //         setTimeout(typeText,TypingSpeed);
-//     }
-// }
-// window.addEventListener("load", typeText);
+//     }// }
+// window.addEventListener("load", typeText);=
